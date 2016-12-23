5598df01f0b4fbb6bb2055f40542203a1871bde4
Mohamed Bouchenafa <mbouchenafa@smartwavesa.com>
2016-12-23 09:21:03 +0100

    Adding comments

--- clean-docker.sh
+++ clean-docker.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
+set -e
+
 eval "$1 docker rm $($1 docker ps -a  -f 'status=exited' -q --no-trunc)" || true
 eval "$1 docker rmi $($1 docker images --filter 'dangling=true' -q --no-trunc)" || true
