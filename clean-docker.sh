c48548dd00eb5e058265f6622009664cc3b88459
antoine palavioux <apalavioux@smartwavesa.com>
2017-08-30 14:49:39 +0200

    added else

--- clean-docker.sh
+++ clean-docker.sh
@@ -2,12 +2,11 @@
 
 set -e
 
-usage="Usage:	clean-docker -s \n
+usage="Usage:	clean-docker -s -n image_name\n
 -s : to execute docker build with sudo \n
 -n image_name  (optional) : the image's name \n"
 
 sudo=""
-image_name=""
 
 while getopts 's:n:' opt; do
     case $opt in
@@ -32,13 +31,15 @@ $2 \n" 1>&2
 	exit 1
 }
 
-eval "echo $image_name"
 eval "$sudo docker rm $($sudo docker ps -a  -f 'status=exited' -q --no-trunc)" || true
 eval "$sudo docker rmi $($sudo docker images --filter 'dangling=true' -q --no-trunc)" || true
+
 if [ ! -z "$image_name" ]
 then
 	get_img_id_local="docker images -q $image_name"
 	img_id_local=$(eval "$get_img_id_local")
 	eval "docker rmi -f $img_id_local"
+else
+	echo "image_name parameter is empty"
 fi
 exit 0
