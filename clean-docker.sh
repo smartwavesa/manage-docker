20a1e28bbc41b1201d2c6c3d6bf81f6d1da878ca
antoine palavioux <apalavioux@smartwavesa.com>
2017-08-30 13:43:20 +0200

    upd

--- clean-docker.sh
+++ clean-docker.sh
@@ -3,13 +3,15 @@
 set -e
 
 usage="Usage:	clean-docker -s \n
--s : to execute docker build with sudo \n"
+-s : to execute docker build with sudo \n
+-n image_name  (mandatory) : the image's name \n"
 
 sudo=""
 
 while getopts 's' opt; do
     case $opt in
 		s)  sudo="sudo" ;;
+		n)  image_name="$OPTARG"    ;;
         *)  exit 1            ;;
     esac
 done
@@ -32,5 +34,10 @@ $2 \n" 1>&2
 
 eval "$sudo docker rm $($sudo docker ps -a  -f 'status=exited' -q --no-trunc)" || true
 eval "$sudo docker rmi $($sudo docker images --filter 'dangling=true' -q --no-trunc)" || true
-
+if [ ! -z "$image_name" ]
+then
+	get_img_id_local="docker images -q $image_name"
+	img_id_local=$(eval "$get_img_id_local")
+	eval "docker rmi -f $img_id_local"
+fi
 exit 0
