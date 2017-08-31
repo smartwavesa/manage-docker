94cc6c2bbccc385cb631c4fa31ba377ed446af70
antoine palavioux <apalavioux@smartwavesa.com>
2017-08-31 11:06:29 +0200

    changed for substring

--- restart-ecs-service.sh
+++ restart-ecs-service.sh
@@ -82,8 +82,9 @@ echo "RUNNING TASKS on $service_name ARE : $taskArns"
 
 for task in $taskArns
 do
-	if [[ $task =~ [a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12} ]]; then
-		echo ${BASH_REMATCH[1]}
+	if [[ $task == *"task"* ]]; then
+		task_clean=${task_clean:40:36}
+		echo "task: $task_clean"
 	fi
 done
 
@@ -92,8 +93,8 @@ then
 	for task in $taskArns
 	do
 		if [[ $task == *"task"* ]]; then
-			task_clean=$task | cut -d'/' -f 2
-			echo "task_clean: $task_clean"
+			task_clean=${task_clean:40:36}
+			echo "task: $task_clean"
 			stop_task=`$ecs_cmd  stop-task --cluster $cluster --task $task_clean`
 		fi
 	done
