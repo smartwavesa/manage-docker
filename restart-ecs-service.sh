ef48d492b001c114b2c11dfd79e8ca46e421020f
antoine palavioux <apalavioux@smartwavesa.com>
2017-08-31 10:16:19 +0200

    update on bash typo

--- restart-ecs-service.sh
+++ restart-ecs-service.sh
@@ -75,10 +75,8 @@ ecs_cmd="$sudo docker run --rm  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_S
 
 ecs_list_tasks="$ecs_cmd list-tasks --cluster $cluster --service-name $service_name"
 
-echo "$ecs_list_tasks"
-
-#taskArns=`$ecs_list_tasks`| jq -r '.taskArns'
-taskArns=$(eval "$ecs_list_tasks" | cut -d'/' -f 2)
+taskArns=$(eval "$ecs_list_tasks" | jq -r '.taskArns')
+#taskArns=$(eval "$ecs_list_tasks" | cut -d'/' -f 2)
 
 echo "RUNNING TASKS on $service_name ARE : $taskArns"
 
